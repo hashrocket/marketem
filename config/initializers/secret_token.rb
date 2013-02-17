afcb1a980e898b39eb4c8ed49a9951cf1615cf0e
Andy Borsz and Micah Cooper <dev+andybz+mrmicahcooper@hashrocket.com>
2013-02-17 10:50:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marketem::Application.config.secret_token = '860e14b7b7b76696ccaaa0356dc3e503cb5c88482a992182724dbc95332370dc224a075115f6db4bd43b2f4760c0f2cea47ed80a6d51fa5b1165720c832bb4a4'
