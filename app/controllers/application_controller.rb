19871595a21954a155afec0996e79eda16cbdf57
Andy Borsz <andy.borsz@gmail.com>
2013-02-17 21:31:25 -0500

    Use environment vairables for basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with(name: 'marketem', password: 'password')
+  http_basic_authenticate_with(
+      name: ENV["BASIC_AUTH_NAME"],
+      password: ENV["BASIC_AUTH_PASSWORD"])
 end
