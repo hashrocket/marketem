b0419a79a93b039e77545ab116dbd697de5283bd
Andy Borsz <andy.borsz@gmail.com>
2013-02-17 23:50:57 -0500

    fixup-environment-variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   http_basic_authenticate_with(
-      name: ENV["BASIC_AUTH_NAME"],
-      password: ENV["BASIC_AUTH_PASSWORD"])
+    name: ENV["BASIC_AUTH_NAME"],
+    password: ENV["BASIC_AUTH_PASSWORD"])
 end
