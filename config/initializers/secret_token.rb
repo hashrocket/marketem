221e06cebed95b99afe8bc6541776324bcfff6ce
Andy Borsz and Travis Anderson <dev+andybz+syntaxritual@hashrocket.com>
2013-06-19 11:08:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marketem::Application.config.secret_key_base = 'ad1be0b0d3c4e644bf4ec2cc294cb8651412c01d490114cca47e5676b9fb198b99ded995a31a4f4e7d717b0b1f63ef4f5de2bbc706397738f8c950cd160fb7f4'
